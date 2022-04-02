6c166e773ca31cb99727e74515d2fdb7be872866
Miles <milescumiskey@gmail.com>
2022-04-02 11:14:00 -0400

    Initial Commit

---
+++ Donsol/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Donsol
+//
+//  Created by Miles Cumiskey on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Donsol/DonsolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonsolApp.swift
+//  Donsol
+//
+//  Created by Miles Cumiskey on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DonsolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
